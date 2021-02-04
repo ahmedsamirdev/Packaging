45b8640fc5903cbaacf9aa1a92fe217db5a2e9a1
unknown <ahmedsamir551@gmail.com>
2021-02-04 02:15:21 +0200

    responsive

--- components/Ourpartners.js
+++ components/Ourpartners.js
@@ -1,7 +1,6 @@
 import "./Ourpartners.module.css";
 
 function Ourpartners() {
-
   return (
     <>
       <div className="ourpartners_title">
@@ -10,12 +9,23 @@ function Ourpartners() {
       </div>
 
       <div className="partners_logos">
-        <img src="/partner1.png" alt="" /> <div class="vl"></div>
-        <img src="/partner2.png" alt="" /> <div class="vl"></div>
-        <img src="/partner3.png" alt="" /> <div class="vl"></div>
-        <img src="/partner4.png" alt="" /> <div class="vl"></div>
-        <img src="/partner5.png" alt="" /> <div class="vl"></div>
+        <img src="/partner1.png" alt="" /> <div className="vl"></div>
+        <img src="/partner2.png" alt="" /> <div className="vl"></div>
+        <img src="/partner3.png" alt="" /> <div className="vl"></div>
+        <img src="/partner4.png" alt="" /> <div className="vl"></div>
+        <img src="/partner5.png" alt="" /> <div className="vl"></div>
         <img src="/partner6.png" alt="" />
+      </div>
+
+      <div className="ourpartners_slider">
+        <div className="slider">
+          <img src="/partner1.png" alt="Slide 1" />
+          <img src="/partner2.png" alt="Slide 2" />
+          <img src="/partner3.png" alt="Slide 3" />
+          <img src="/partner4.png" alt="Slide 4" />
+          <img src="/partner5.png" alt="Slide 5" />
+          <img src="/partner6.png" alt="Slide 6" />
+        </div>
       </div>
     </>
   );
